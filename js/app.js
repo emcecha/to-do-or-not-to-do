3dbec4b4256b748a04322588fa2f390a744f3712
Michał Chudy <chudymw@gmail.com>
2019-02-13 17:34:18 +0100

    ostylowanie nagłówka strony - logo, pasek wyszukiwania, przycisk dodawania zadań

--- js/app.js
+++ js/app.js
@@ -10,26 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
     //
     // })
 
-    var newTask = document.querySelector("textarea");
     var createButton = document.querySelector(".button--create");
     var body = document.querySelector("body");
     var tasksList = document.querySelector(".tasks__list");
 
-    createButton.addEventListener("click", function () {
+    function createNewTaskForm() {
 
         var shadowBox = document.createElement("div");
         shadowBox.classList.add("shadowbox");
 
-        var form = document.createElement("div");
+        var form = document.createElement("form");
         form.classList.add("shadowbox__form");
         shadowBox.appendChild(form);
+    }
+
+
+    createButton.addEventListener("click", function () {
+
+
 
         var formTitle = document.createElement("h2");
         formTitle.innerText = "Wpisz co i kiedy masz do zrobienia:"
         formTitle.classList.add("shadowbox__form__title");
         form.appendChild(formTitle);
 
-        var inputBox = document.createElement("div");
+        var inputBox = document.createElement("formfield");
         inputBox.classList.add("shadowbox__form__inputbox");
         form.appendChild(inputBox);
 
@@ -46,38 +51,55 @@ document.addEventListener("DOMContentLoaded", function () {
         var addButton = document.createElement("button");
         addButton.classList.add("button");
         addButton.classList.add("button--add");
+        addButton.type = "button";
         addButton.innerText = "+";
         form.appendChild(addButton);
         addButton.addEventListener("click", function () {
 
             var taskTextInput = document.querySelector(".shadowbox__form__inputbox__text");
-            var newTask = document.createElement("li");
-            newTask.innerText = taskTextInput.value;
+            var taskDateInput = document.querySelector(".shadowbox__form__inputbox__date");
 
-            tasksList.appendChild(newTask);
+            if (taskTextInput.value.length > 0) {
 
-            closeShadowBox();
+                var newTaskBox = document.createElement("li");
+                newTaskBox.classList.add("tasks__list__item");
+
+                var newTaskText = document.createElement("p");
+                newTaskText.classList.add("tasks__list__item__text");
+                newTaskText.innerText = taskTextInput.value;
+                newTaskBox.appendChild(newTaskText);
+
+                var newTaskDate = document.createElement("p");
+                newTaskDate.classList.add("tasks__list__item__date");
+                newTaskDate.innerText = taskDateInput.value;
+                console.log(new Date(taskDateInput.value).getTime());
+                newTaskBox.appendChild(newTaskDate);
+
+                tasksList.appendChild(newTaskBox);
+                deleteShadowBox();
+
+            }
 
         });
 
         var closeButton = document.createElement("button");
         closeButton.classList.add("button");
         closeButton.classList.add("button--close");
+        closeButton.type = "button";
         closeButton.innerText = "x";
         form.appendChild(closeButton);
-        closeButton.addEventListener("click", closeShadowBox);
+        closeButton.addEventListener("click", deleteShadowBox);
         body.appendChild(shadowBox);
 
     })
 
-    // console.log(newTask);
 
     function resizeHeight(event) {
         this.style.height = "auto";
         this.style.height = this.scrollHeight + 5 + "px";
     };
 
-    function closeShadowBox(event) {
+    function deleteShadowBox(event) {
         var shadowBox = document.querySelector(".shadowbox");
         body.removeChild(shadowBox);
     }
